ee17b36b1cf0173ba86d01888034c8fb6708f229
Ubuntu <kit214@labKMLW6H.vvfyilsrawwevfva4hjuedeo3h.px.internal.cloudapp.net>
2024-11-03 01:59:39 +0000

    css implemented

--- views/AddCarView.php
+++ views/AddCarView.php
@@ -4,19 +4,33 @@ class AddCarView
     public function output()
     {
         ?>
-        <h1>Add Car</h1>
-        <form action="index.php?controller=car&action=add" method="post">
-            <label for="race">Suitability Race:</label>
-            <input type="text" id="race" name="race" required>
-            <br>
-            <label for="street">Suitability Street:</label>
-            <input type="text" id="street" name="street" required>
-            <br>
-            <label for="reliability">Reliability:</label>
-            <input type="text" id="reliability" name="reliability" required>
-            <br>
-            <input type="submit" value="Add Car">
-        </form>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Add Car</title>
+            <link rel="stylesheet" href="css/styles.css"> <!-- Include your CSS file here -->
+        </head>
+
+        <body>
+            <h1>Add Car</h1>
+            <form action="index.php?controller=car&action=add" method="post">
+                <label for="race">Suitability Race:</label>
+                <input type="text" id="race" name="race" required>
+                <br>
+                <label for="street">Suitability Street:</label>
+                <input type="text" id="street" name="street" required>
+                <br>
+                <label for="reliability">Reliability:</label>
+                <input type="text" id="reliability" name="reliability" required>
+                <br>
+                <input type="submit" value="Add Car">
+            </form>
+        </body>
+
+        </html>
         <?php
     }
 }
--- views/DriverListView.php
+++ views/DriverListView.php
@@ -6,30 +6,44 @@ class DriverListView
     public function output($drivers)
     {
         ?>
-        <h1>Drivers</h1>
-        <a href="?controller=driver&action=toAdd">Add Driver</a>
-        <table>
-            <tr>
-                <th>Number</th>
-                <th>Name</th>
-                <th>Short Name</th>
-                <th>Race Skill</th>
-                <th>Street Skill</th>
-                <th>Actions</th>
-            </tr>
-            <?php foreach ($drivers as $driver) { ?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Add Car</title>
+            <link rel="stylesheet" href="../css/style.css">
+        </head>
+
+        <body>
+            <h1>Drivers</h1>
+            <a href="?controller=driver&action=toAdd">Add Driver</a>
+            <table>
                 <tr>
-                    <td><?php echo $driver['number']; ?></td>
-                    <td><?php echo $driver['name']; ?></td>
-                    <td><?php echo $driver['shortName']; ?></td>
-                    <td><?php echo $driver['skill']['race']; ?></td>
-                    <td><?php echo $driver['skill']['street']; ?></td>
-                    <td>
-                        <a href="?controller=driver&action=delete&number=<?php echo $driver['number']; ?>">Delete</a>
-                    </td>
+                    <th>Number</th>
+                    <th>Name</th>
+                    <th>Short Name</th>
+                    <th>Race Skill</th>
+                    <th>Street Skill</th>
+                    <th>Actions</th>
                 </tr>
-            <?php } ?>
-        </table>
+                <?php foreach ($drivers as $driver) { ?>
+                    <tr>
+                        <td><?php echo $driver['number']; ?></td>
+                        <td><?php echo $driver['name']; ?></td>
+                        <td><?php echo $driver['shortName']; ?></td>
+                        <td><?php echo $driver['skill']['race']; ?></td>
+                        <td><?php echo $driver['skill']['street']; ?></td>
+                        <td>
+                            <a href="?controller=driver&action=delete&number=<?php echo $driver['number']; ?>">Delete</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </body>
+
+        </html>
         <?php
     }
 }
